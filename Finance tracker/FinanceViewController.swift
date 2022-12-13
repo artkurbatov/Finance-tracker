9d247919053f72e076567e17e40ba3d8ecddaca5
KurbatovIOS <akurbatov.work@gmail.com>
2022-12-13 22:00:06 +0300

    basic setup

--- Finance tracker/FinanceViewController.swift
+++ Finance tracker/FinanceViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FinanceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
     }
 
 
