ec28ac5dd2fec232e3732a79563537d818529e14
KurbatovIOS <akurbatov.work@gmail.com>
2022-12-13 22:47:45 +0300

    elements addition

--- Finance tracker/FinanceViewController.swift
+++ Finance tracker/FinanceViewController.swift
@@ -8,13 +8,72 @@
 import UIKit
 
 class FinanceViewController: UIViewController {
+    
+    private let titleLabel = UILabel()
+    private let addButton = UIButton()
+    private let historyTableView = UITableView()
+    private let periodPicker = UIPickerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+        historyTableView.register(UITableViewCell.self, forCellReuseIdentifier: "financeCell")
+        
+        setupTitleLabel()
+        setupAddButton()
+    }
+    
+    // MARK: - Views setup
+
+    private func setupTitleLabel() {
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.text = "Finance"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+    }
+    
+    private func setupAddButton() {
+        
+        view.addSubview(addButton)
+        
+        addButton.setImage(UIImage(systemName: "plus.forwardslash.minus"), for: .normal)
+        
+        
+        addButton.tintColor = .black
+        
+        addButton.addTarget(self, action: #selector(addButtonAction), for: .touchUpInside)
+        
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        addButton.topAnchor.constraint(equalTo: titleLabel.topAnchor).isActive = true
     }
 
+    @objc private func addButtonAction() {
+        print("add")
+    }
+}
 
+
+extension FinanceViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = historyTableView.dequeueReusableCell(withIdentifier: "financeCell", for: indexPath)
+        return cell
+    }
 }
 
--- Finance tracker/SceneDelegate.swift
+++ Finance tracker/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarViewController()
+        window?.rootViewController = FinanceViewController()
         window?.makeKeyAndVisible()
     }
 
